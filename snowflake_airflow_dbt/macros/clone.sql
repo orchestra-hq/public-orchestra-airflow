fede786af7cd14d63d5c543e0f5ec87c15705368
ORCHESTRATIONMASTER <127905842+ORCHESTRATIONMASTER@users.noreply.github.com>
2023-08-24 15:42:33 +0100

    Elite clone post hook

--- snowflake_airflow_dbt/macros/clone.sql
+++ snowflake_airflow_dbt/macros/clone.sql
@@ -1,3 +1,6 @@
 {% macro clone() %}
-create table if not exists SNOWFLAKE_WORKING.PUBLIC_prod.prod_snowflake_orders_clone  clone snowflake_working.public_aggregated.snowflake_orders
+{% set identifier_parts = this.identifier.split('.') %}
+{% set table_name = identifier_parts[-1] %}
+create or replace table SNOWFLAKE_WORKING.PUBLIC_prod.{{table_name}}_clone
+clone snowflake_working.public_aggregated.{{table_name}}
 {% endmacro %}
